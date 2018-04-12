9d6cc559361ca44047cadc7ff87551b039efe387
Villu Vaimets <villu.vaimets@resys.io>
2018-04-12 17:08:53 +0300

    Tests

--- nwb.config.js
+++ nwb.config.js
@@ -8,5 +8,13 @@ module.exports = {
         chunkFilename: '[name].js'
       }
     }
+  },
+  karma: {
+    testFiles: ['tests/**/*Test.js'],
+    frameworks: ['mocha', 'chai', 'chai-immutable'],
+    plugins: [
+      require('karma-chai-plugins'),
+      require('karma-chai-immutable')
+    ]
   }
 }
