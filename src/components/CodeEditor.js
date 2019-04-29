df4e3e7bd9c60f293cc125d5ca5392e4afa17b07
Villu Vaimets <villu.vaimets@resys.io>
2019-04-29 14:16:35 +0300

    Simple hinting

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import CodeMirrorIntegration from 'codemirror';
 import {debounce} from 'lodash';
+import delHinter from '../del/hinting';
+import {connect} from 'react-redux';
 
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/addon/lint/lint.css';
@@ -16,14 +18,42 @@ class CodeEditor extends Component {
     }
   }, 250);
 
+  getHints(editor, options) {
+    const cursor = editor.getCursor();
+    const line = editor.getLine(cursor.line);
+    const token = editor.getTokenAt(cursor);
+    const pos = {
+      id: cursor.line,
+      content: token.string,
+      at: cursor.ch,
+      isEmpty: token.string.length === 0 || !token.string.trim()
+    };
+
+    return {
+      list: options.getList(pos),
+      from: CodeMirrorIntegration.Pos(cursor.line, pos.isEmpty ? token.end : token.start ),
+      to: CodeMirrorIntegration.Pos(cursor.line, token.end)
+    };
+  }
+
   componentDidMount() {
     const element = this.element;
     if (element) {
+
+        const hintOptions = {
+          completeSingle: false,
+          hint: this.getHints,
+          getList: this.props.hinter
+        }
 
         const editor = CodeMirrorIntegration.fromTextArea(element, {
           mode: 'del',
           lineNumbers: false,
           tabSize: 2,
+          extraKeys: {
+            'Ctrl-Space': 'autocomplete'
+          },
+          hintOptions
         });
 
         editor.on('change', e => this.handleChange(e.getValue()));
@@ -36,6 +66,15 @@ class CodeEditor extends Component {
 
 }
 
+const CodeEditorConnected = connect(
+  state => ({
+    hinter: context => delHinter(context, state.dialobComposer.form)
+  }),
+  {
+  }
+)(CodeEditor);
+
 export {
-  CodeEditor as default
+  CodeEditorConnected as default,
+  CodeEditor
 };
