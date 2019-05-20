b0043907f5f12bda00101eb40a6453cc8981885a
Oskar Köök <oskarkook@maatriks.ee>
2019-05-20 17:08:58 +0300

    remove old export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ export { SessionContext } from './context/sessionContext';
 export { useFillItem } from './hooks/useFillItem';
 export { useFillSession } from './hooks/useFillSession';
 export { Session, SessionProps } from './Session';
-export { SessionComponents } from './sessionComponents';
 
