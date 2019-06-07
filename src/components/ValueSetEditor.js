280cfb9a438718b7caaa128e88fa1ed79fc49ae2
Villu Vaimets <villu.vaimets@resys.io>
2019-06-07 17:53:24 +0300

    #51 show valueset errors in valueset editor

--- src/components/ValueSetEditor.js
+++ src/components/ValueSetEditor.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {Table, Button, Input} from 'semantic-ui-react';
+import {Table, Button, Input, Message} from 'semantic-ui-react';
 import {connect} from 'react-redux';
 import {findValueset} from '../helpers/utils';
 import {createValueset, createValuesetEntry, updateValuesetEntry, deleteValuesetEntry} from '../actions';
 import * as Defaults from '../defaults';
+import { translateErrorMessage } from '../helpers/utils';
 
 class ValueSetEditor extends Component {
 
@@ -24,20 +25,39 @@ class ValueSetEditor extends Component {
   }
 
   render() {
-      let rows = this.props.getValueset().get('entries') ? this.props.getValueset().get('entries').map((e, i) => this.entryEditor(e, i)) : [];
+      const dedupe = (item, idx, arr) => arr.indexOf(item) === idx;
+
+      const rows = this.props.getValueset().get('entries') ? this.props.getValueset().get('entries').map((e, i) => this.entryEditor(e, i)) : [];
+      const errors = this.props.errors &&
+          this.props.errors
+            .filter(e => e.get('message').startsWith('VALUESET_') && e.get('itemId') === this.props.valueSetId)
+            .groupBy(e => e.get('level'));
+
+      const errorList = errors && errors.get('ERROR') &&
+                <Message attached={errors.get('WARNING') ? true : 'bottom'} error header='Errors'
+                    list={errors.get('ERROR').map(e => translateErrorMessage(e)).toJS().filter(dedupe)} />;
+
+      const warningList = errors && errors.get('WARNING') &&
+                <Message attached='bottom' warning header='Warnings'
+                    list={errors.get('WARNING').map(e => translateErrorMessage(e)).toJS().filter(dedupe)} />;
+
       return (
-        <Table celled>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell collapsing><Button size='tiny' icon='add' onClick={() => this.props.createValuesetEntry(this.props.getValueset().get('id'))} /></Table.HeaderCell>
-            <Table.HeaderCell>Key</Table.HeaderCell>
-            <Table.HeaderCell>Text</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {rows}
-        </Table.Body>
-      </Table>
+      <React.Fragment>
+        <Table celled attached={errorList || warningList ? 'top' : null}>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell collapsing><Button size='tiny' icon='add' onClick={() => this.props.createValuesetEntry(this.props.getValueset().get('id'))} /></Table.HeaderCell>
+              <Table.HeaderCell>Key</Table.HeaderCell>
+              <Table.HeaderCell>Text</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {rows}
+          </Table.Body>
+        </Table>
+        {errorList}
+        {warningList}
+      </React.Fragment>
       );
   }
 }
@@ -45,7 +65,8 @@ class ValueSetEditor extends Component {
 const ValueSetEditorConnected = connect(
   (state, props) => ({
     language: (state.dialobComposer.editor && state.dialobComposer.editor.get('activeLanguage')) || Defaults.FALLBACK_LANGUAGE,
-    get getValueset() { return () => findValueset(state.dialobComposer.form, props.valueSetId); }
+    get getValueset() { return () => findValueset(state.dialobComposer.form, props.valueSetId); },
+    errors: state.dialobComposer.editor && state.dialobComposer.editor.get('errors')
   }), {
     createValueset,
     createValuesetEntry,
