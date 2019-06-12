c9b426a9e86515b0114bbb16c90c511dd733a61f
Villu Vaimets <villu.vaimets@resys.io>
2019-06-12 14:16:52 +0300

    #56 valueset entry row component

--- src/components/ValueSetEditor.js
+++ src/components/ValueSetEditor.js
@@ -6,31 +6,34 @@ import {createValueset, createValuesetEntry, updateValuesetEntry, deleteValueset
 import * as Defaults from '../defaults';
 import { translateErrorMessage } from '../helpers/utils';
 
-class ValueSetEditor extends Component {
-
-  entryEditor(entry, index, valueSetErrors) {
-    const vsId = this.props.valueSetId;
+class EntryRow extends Component {
+  render() {
+    const {entry, index, valueSetErrors, deleteValuesetEntry, updateValuesetEntry, valueSetId, language} = this.props;
     const entryErrors = valueSetErrors && valueSetErrors.find(e => e.get('index') === index || e.get('expression') === entry.get('id'));
     return (
       <Table.Row key={index} error={entryErrors ? true : false}>
         <Table.Cell collapsing>
-          <Button size='tiny' icon='remove' onClick={() => this.props.deleteValuesetEntry(vsId, index)} />
+          <Button size='tiny' icon='remove' onClick={() => deleteValuesetEntry(valueSetId, index)} />
         </Table.Cell>
         <Table.Cell>
-           <Input transparent fluid value={entry.get('id') || ''} onChange={(e) => this.props.updateValuesetEntry(vsId, index, e.target.value, null, null)} />
+            <Input transparent fluid value={entry.get('id') || ''} onChange={(e) => updateValuesetEntry(valueSetId, index, e.target.value, null, null)} />
         </Table.Cell>
         <Table.Cell>
-           <Input transparent fluid value={entry.getIn(['label', this.props.language]) || ''} onChange={(e) => this.props.updateValuesetEntry(vsId, index, null, e.target.value, this.props.language)}/>
+            <Input transparent fluid value={entry.getIn(['label', language]) || ''} onChange={(e) => updateValuesetEntry(valueSetId, index, null, e.target.value, language)}/>
         </Table.Cell>
       </Table.Row>);
   }
+};
+
+class ValueSetEditor extends Component {
 
   render() {
+      const {deleteValuesetEntry, updateValuesetEntry, valueSetId, language} = this.props;
       const dedupe = (item, idx, arr) => arr.indexOf(item) === idx;
 
       const valueSetErrors = this.props.errors &&
           this.props.errors
-            .filter(e => e.get('message').startsWith('VALUESET_') && e.get('itemId') === this.props.valueSetId)
+            .filter(e => e.get('message').startsWith('VALUESET_') && e.get('itemId') === valueSetId)
       const errors = valueSetErrors && valueSetErrors.groupBy(e => e.get('level'));
 
       const errorList = errors && errors.get('ERROR') &&
@@ -41,7 +44,11 @@ class ValueSetEditor extends Component {
                 <Message attached='bottom' warning header='Warnings'
                     list={errors.get('WARNING').map(e => translateErrorMessage(e)).toJS().filter(dedupe)} />;
 
-      const rows = this.props.getValueset().get('entries') ? this.props.getValueset().get('entries').map((e, i) => this.entryEditor(e, i, valueSetErrors)) : [];
+      const rows = this.props.getValueset().get('entries')
+        ? this.props.getValueset().get('entries').map((e, i) =>
+           <EntryRow entry={e} index={i} valueSetErrors={valueSetErrors}
+              deleteValuesetEntry={deleteValuesetEntry} updateValuesetEntry={updateValuesetEntry} valueSetId={valueSetId} language={language} />)
+        : [];
 
       return (
       <React.Fragment>
