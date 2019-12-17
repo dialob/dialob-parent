eed3db145987af4243e9da07341c9213a16a118b
Villu Vaimets <villu.vaimets@resys.io>
2019-12-17 13:26:37 +0200

    Show rule editors only for active items, closes #99

--- src/items/Note.js
+++ src/items/Note.js
@@ -24,15 +24,18 @@ class Note extends Item {
             </Table.Row>
           </Table.Body>
         </Table>
-        <Table onClick={(e) => {e.stopPropagation(); this.setActive(true);}}  celled attached >
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell error={this.getErrors().filter(e => e.get('type') === 'VISIBILITY').size > 0}>
-               <CodeEditor value={this.props.item.get('activeWhen') || ''} onChange={value => this.setAttribute('activeWhen', value)} placeholder='Visibility' readOnly={!this.props.editable} icon='eye' errors={this.getErrors().filter(e => e.get('type') === 'VISIBILITY')}/>
-              </Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
+        {
+          this.props.active &&
+          <Table onClick={(e) => {e.stopPropagation(); this.setActive(true);}}  celled attached >
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell error={this.getErrors().filter(e => e.get('type') === 'VISIBILITY').size > 0}>
+                <CodeEditor value={this.props.item.get('activeWhen') || ''} onChange={value => this.setAttribute('activeWhen', value)} placeholder='Visibility' readOnly={!this.props.editable} icon='eye' errors={this.getErrors().filter(e => e.get('type') === 'VISIBILITY')}/>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        }
         <Segment onClick={(e) => {e.stopPropagation(); this.setActive(true);}}  className={classnames({'composer-active': this.props.active})} attached='bottom'>
           <RichEditor key={editorKey} active={this.props.active} id={editorKey} onChange={(v) => this.setAttribute('label', v, this.props.language)} defaultValue={this.props.item.getIn(['label', this.props.language])} placeholder='Write note text...' />
         </Segment>
