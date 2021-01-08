fbac14e1d5b1e3b7a705bd5b08cd2a26006e1d10
Oskar Köök <oskar@maatriks.ee>
2021-01-08 19:45:02 +0100

    separate state logic

--- src/session.ts
+++ src/session.ts
@@ -1,33 +1,17 @@
 import produce from 'immer';
-import { Action, ErrorAction, ItemAction, ItemType, ValueSetAction } from './actions';
+import { Action } from './actions';
 import { DialobError } from './error';
+import { updateState, initState, SessionError, SessionItem, SessionState, SessionValueSet } from './state';
 import { onSyncFn, SyncQueue } from './sync-queue';
 import { Transport } from './transport';
 
-export type SessionItem<T extends ItemType = ItemType> = ItemAction<T>['item'];
-export type SessionError = ErrorAction['error'];
-export type SessionValueSet = ValueSetAction['valueSet'];
-
-export interface SessionState {
-  items: Record<string, SessionItem>;
-  reverseItemMap: Record<string, Set<string>>;
-  valueSets: Record<string, SessionValueSet>;
-  errors: Record<string, SessionError[]>;
-  locale?: string;
-  complete: boolean;
-  variables: {
-    [id: string]: any;
-  }
-};
-
+type Event = 'update' | 'sync' | 'error';
 export type onUpdateFn = () => void;
 export type onErrorFn = (type: 'CLIENT' | 'SYNC' | 'SYNC-REPEATED', error: DialobError) => void;
 
 export interface SessionOptions {
   syncWait?: number;
 }
-
-type Event = 'update' | 'sync' | 'error';
 
 export class Session {
   private state: SessionState;
@@ -42,126 +26,23 @@ export class Session {
   };
 
   constructor(id: string, transport: Transport, options: SessionOptions = {}) {
-    this.state = {
-      items: {},
-      reverseItemMap: {},
-      valueSets: {},
-      errors: {},
-      complete: false,
-      variables: {}
-    };
+    this.state = initState();
     this.syncQueue = new SyncQueue(id, transport, options.syncWait || 250);
     this.syncQueue.on('sync', (type, response) => {
       if(type === 'DONE' && response?.actions) {
-        this.applyActions(response.actions);
+        this.state = updateState(this.state, response.actions);
+        this.listeners.update.forEach(l => l());
       }
     });
   }
 
-  /** STATE LOGIC */
-  private insertReverseRef(state: SessionState, parentId: string, refIds: string[]) {
-    for(const refId of refIds) {
-      if(!state.reverseItemMap[refId]) {
-        state.reverseItemMap[refId] = new Set();
-      }
-      state.reverseItemMap[refId].add(parentId);
-    }
-  }
-
-  private applyActions(actions: Action[]): SessionState {
-    this.state = produce(this.state, state => {
-      for(const action of actions) {
-        if(action.type === 'RESET') {
-          state.items = {};
-          state.reverseItemMap = {};
-          state.valueSets = {};
-          state.errors = {};
-          state.locale = undefined;
-          state.complete = false;
-        } else if(action.type === 'ANSWER') {
-          const answer = state.items[action.id];
-          if(!answer) throw new DialobError(`No item found with id '${action.id}'`);
-          if(answer.type === 'questionnaire' || answer.type === 'group' || answer.type === 'surveygroup' || answer.type === 'note') {
-            throw new DialobError(`Item '${action.id}' is not an answer!`);
-          }
-
-          answer.value = action.answer;
-        } else if(action.type === 'ITEM') {
-          const item = action.item;
-
-          if (action.item.type === 'context' || action.item.type === 'variable') {
-            this.state.variables[action.item.id] = action.item.value;
-          } else {
-            state.items[item.id] = item;
-
-            if('items' in item && item.items) {
-              this.insertReverseRef(state, item.id, item.items);
-            }
-          }
-        } else if(action.type === 'ERROR') {
-          const error = action.error;
-          if(!state.errors[error.id]) {
-            state.errors[error.id] = [];
-          }
-
-          state.errors[error.id].push(error);
-        } else if(action.type === 'LOCALE') {
-          state.locale = action.value;
-        } else if(action.type === 'VALUE_SET') {
-          state.valueSets[action.valueSet.id] = action.valueSet;
-        } else if(action.type === 'REMOVE_ITEMS') {
-          for(const id of action.ids) {
-            delete state.items[id];
-            delete state.errors[id];
-
-            if(state.reverseItemMap[id]) {
-              state.reverseItemMap[id].forEach(reference => {
-                const referencedItem: any = state.items[reference];
-                if(!referencedItem || !referencedItem['items']) return;
-                const idx = referencedItem.items.indexOf(reference);
-                if(idx === -1) return;
-                referencedItem.items.splice(idx, 1);
-              });
-            }
-            delete state.reverseItemMap[id];
-          }
-        } else if(action.type === 'ADD_ROW') {
-          // Wait for server response
-        } else if(action.type === 'DELETE_ROW') {
-          delete state.items[action.id];
-        } else if(action.type === 'COMPLETE') {
-          state.complete = true;
-        } else if(action.type === 'NEXT') {
-          // Wait for server response
-        } else if(action.type === 'PREVIOUS') {
-          // Wait for server response
-        } else if (action.type === 'GOTO') {
-          // Wait for server response
-        } else if (action.type === 'SET_LOCALE') {
-          // Wait for server response
-        } else if(action.type === 'REMOVE_ERROR') {
-          const error = action.error;
-          const itemErrors = state.errors[error.id];
-          if(itemErrors) {
-            const errorIdx = itemErrors.findIndex(e => e.code === error.code);
-            if(errorIdx !== -1) {
-              itemErrors.splice(errorIdx, 1);
-            }
-          }
-        } else if(action.type === 'REMOVE_VALUE_SETS') {
-          for(const id of action.ids) {
-            delete state.valueSets[id];
-          }
-        } else {
-          this.handleError(new DialobError('Unexpected action type!'));
-        }
-      }
-    });
-
+  applyActions(actions: Action[]): SessionState {
+    this.state = updateState(this.state, actions);
     this.listeners.update.forEach(l => l());
     return this.state;
   }
 
+  /** STATE LOGIC */
   public getItem(id: string): SessionItem | undefined {
     return this.state.items[id];
   }
---
+++ src/state.ts
@@ -0,0 +1,125 @@
+import produce from 'immer';
+import { DialobError } from './error';
+import { Action, ErrorAction, ItemAction, ItemType, ValueSetAction } from './actions';
+
+export type SessionItem<T extends ItemType = ItemType> = ItemAction<T>['item'];
+export type SessionError = ErrorAction['error'];
+export type SessionValueSet = ValueSetAction['valueSet'];
+
+export interface SessionState {
+  items: Record<string, SessionItem>;
+  reverseItemMap: Record<string, Set<string>>;
+  valueSets: Record<string, SessionValueSet>;
+  errors: Record<string, SessionError[]>;
+  locale?: string;
+  complete: boolean;
+  variables: {
+    [id: string]: any;
+  }
+};
+
+export function initState(): SessionState {
+  return {
+    items: {},
+    reverseItemMap: {},
+    valueSets: {},
+    errors: {},
+    complete: false,
+    variables: {}
+  };
+}
+
+function insertReverseRef(state: SessionState, parentId: string, refIds: string[]) {
+    for(const refId of refIds) {
+      if(!state.reverseItemMap[refId]) {
+        state.reverseItemMap[refId] = new Set();
+      }
+      state.reverseItemMap[refId].add(parentId);
+    }
+  }
+
+export function updateState(state: SessionState, actions: Action[]): SessionState {
+  return produce(state, state => {
+    for(const action of actions) {
+      if(action.type === 'RESET') {
+        return initState();
+      } else if(action.type === 'ANSWER') {
+        const answer = state.items[action.id];
+        if(!answer) throw new DialobError(`No item found with id '${action.id}'`);
+        if(answer.type === 'questionnaire' || answer.type === 'group' || answer.type === 'surveygroup' || answer.type === 'note') {
+          throw new DialobError(`Item '${action.id}' is not an answer!`);
+        }
+
+        answer.value = action.answer;
+      } else if(action.type === 'ITEM') {
+        const item = action.item;
+
+        if (action.item.type === 'context' || action.item.type === 'variable') {
+          state.variables[action.item.id] = action.item.value;
+        } else {
+          state.items[item.id] = item;
+
+          if('items' in item && item.items) {
+            insertReverseRef(state, item.id, item.items);
+          }
+        }
+      } else if(action.type === 'ERROR') {
+        const error = action.error;
+        if(!state.errors[error.id]) {
+          state.errors[error.id] = [];
+        }
+
+        state.errors[error.id].push(error);
+      } else if(action.type === 'LOCALE') {
+        state.locale = action.value;
+      } else if(action.type === 'VALUE_SET') {
+        state.valueSets[action.valueSet.id] = action.valueSet;
+      } else if(action.type === 'REMOVE_ITEMS') {
+        for(const id of action.ids) {
+          delete state.items[id];
+          delete state.errors[id];
+
+          if(state.reverseItemMap[id]) {
+            state.reverseItemMap[id].forEach(reference => {
+              const referencedItem: any = state.items[reference];
+              if(!referencedItem || !referencedItem['items']) return;
+              const idx = referencedItem.items.indexOf(reference);
+              if(idx === -1) return;
+              referencedItem.items.splice(idx, 1);
+            });
+          }
+          delete state.reverseItemMap[id];
+        }
+      } else if(action.type === 'ADD_ROW') {
+        // Wait for server response
+      } else if(action.type === 'DELETE_ROW') {
+        delete state.items[action.id];
+      } else if(action.type === 'COMPLETE') {
+        state.complete = true;
+      } else if(action.type === 'NEXT') {
+        // Wait for server response
+      } else if(action.type === 'PREVIOUS') {
+        // Wait for server response
+      } else if (action.type === 'GOTO') {
+        // Wait for server response
+      } else if (action.type === 'SET_LOCALE') {
+        // Wait for server response
+      } else if(action.type === 'REMOVE_ERROR') {
+        const error = action.error;
+        const itemErrors = state.errors[error.id];
+        if(itemErrors) {
+          const errorIdx = itemErrors.findIndex(e => e.code === error.code);
+          if(errorIdx !== -1) {
+            itemErrors.splice(errorIdx, 1);
+          }
+        }
+      } else if(action.type === 'REMOVE_VALUE_SETS') {
+        for(const id of action.ids) {
+          delete state.valueSets[id];
+        }
+      } else {
+        throw new DialobError('Unexpected action type!');
+      }
+    }
+  });
+}
