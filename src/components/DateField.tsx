60c625c65e9049ea0f58a9088d287fed32eb6707
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2021-07-15 11:51:21 +0300

    datefield test

--- src/components/DateField.tsx
+++ src/components/DateField.tsx
@@ -1,23 +1,25 @@
+import React from 'react';
 import { ItemAction, SessionError } from '@dialob/fill-api';
 import { useFillActions, useFillLocale } from '@dialob/fill-react';
-import React from 'react';
-import {DatePicker} from '@material-ui/pickers';
+import { TextField } from '@material-ui/core';
+import DatePicker from '@material-ui/lab/DatePicker';
 import moment from 'moment';
 import { RenderErrors } from './helpers';
 import { DescriptionWrapper } from './DescriptionWrapper';
+
 
 export interface DateFieldProps {
   datefield: ItemAction<'date'>['item'];
   errors: SessionError[];
 };
 export const DateField: React.FC<DateFieldProps> = ({ datefield, errors }) => {
-  const {setAnswer} = useFillActions();
+  const { setAnswer } = useFillActions();
   const locale = useFillLocale();
   const handleChange = (value: any) => {
     setAnswer(datefield.id, (value as moment.Moment).format('YYYY-MM-DD'));
   }
   const value = datefield.value ? datefield.value as string : null;
-  const format =  moment.localeData(locale).longDateFormat('LL');
+  const format = moment.localeData(locale).longDateFormat('LL');
   return (
     <DescriptionWrapper text={datefield.description} title={datefield.label}>
       <DatePicker
@@ -30,7 +32,9 @@ export const DateField: React.FC<DateFieldProps> = ({ datefield, errors }) => {
         required={datefield.required}
         error={errors.length > 0}
         helperText={<RenderErrors errors={errors} />}
-       />
-    </DescriptionWrapper>
+        renderInput={(props) => (<TextField {...props} />)}
+      />
+      
+    </DescriptionWrapper> 
   );
 }
