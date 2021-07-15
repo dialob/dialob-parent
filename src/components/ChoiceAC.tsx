220d6a4d08c053f37e49c1b0f92dbc5adfefd624
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2021-07-15 11:33:56 +0300

    update choiceAC

--- src/components/ChoiceAC.tsx
+++ src/components/ChoiceAC.tsx
@@ -2,7 +2,7 @@ import { ItemAction, SessionError } from '@dialob/fill-api';
 import { useFillActions, useFillValueSet } from '@dialob/fill-react';
 import React from 'react';
 import { TextField } from '@material-ui/core';
-import { Autocomplete } from '@material-ui/lab';
+import Autocomplete from '@material-ui/core/Autocomplete';
 import { RenderErrors } from './helpers';
 import { DescriptionWrapper } from './DescriptionWrapper';
 
@@ -17,7 +17,7 @@ interface ValueSetEntry {
 };
 
 export const ChoiceAC: React.FC<ChoiceACProps> = ({ choice, errors }) => {
-  const {setAnswer} = useFillActions();
+  const { setAnswer } = useFillActions();
   const valueSet = useFillValueSet(choice.valueSetId);
   const entries: ValueSetEntry[] = valueSet?.entries ? valueSet.entries : [];
 
@@ -26,15 +26,15 @@ export const ChoiceAC: React.FC<ChoiceACProps> = ({ choice, errors }) => {
       <Autocomplete
         options={entries}
         getOptionLabel={c => c?.value || ''}
-        value={choice.value ? entries.find(e => e.key === choice.value) : {key:'',value:''} as ValueSetEntry}
-        getOptionSelected={(option, value) => option?.key === value?.key}
+        value={choice.value ? entries.find(e => e.key === choice.value) : { key: '', value: '' } as ValueSetEntry}
+        isOptionEqualToValue={(option, value) => option?.key === value?.key}
         fullWidth
         autoComplete
-        onChange = {(event: any, newValue?: ValueSetEntry | undefined | null) => {
+        onChange={(event: any, newValue?: ValueSetEntry | undefined | null) => {
           setAnswer(choice.id, newValue?.key);
         }}
-        renderInput={params => <TextField {...params} inputProps={{...params.inputProps, autoComplete: 'new-password'}} label={choice.label} error={errors.length > 0} 
-        helperText={<RenderErrors errors={errors} />}
+        renderInput={params => <TextField {...params} inputProps={{ ...params.inputProps, autoComplete: 'new-password' }} label={choice.label} error={errors.length > 0}
+          helperText={<RenderErrors errors={errors} />}
         />}
       />
     </DescriptionWrapper>
