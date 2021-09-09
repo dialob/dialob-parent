b29fcbdb9096ff9aeb40b6799b37719e5e92d98e
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2021-09-09 12:02:36 +0300

    init

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.tsx'],
+  
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/preset-create-react-app'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+};
