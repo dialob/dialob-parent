76897e7de2f44e1178007ed6bfa5763142a2c79e
Villu Vaimets <villu.vaimets@resys.io>
2023-02-14 13:01:16 +0200

    #20 Build amd push docker images (#27)
    * #20 Build amd push docker images
    
    * #20 Build amd push docker images
    
    * #20 updated to setup-java
    
    * #20 updated to setup-java

---
+++ dialob-build-parent/docker.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# Copyright © 2015 - 2021 ReSys (info@dialob.io)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+echo "Build and push docker images"
+
+DIALOB_BOOT_IMAGE=docker.io/resys/dialob-boot
+DIALOB_SESSION_IMAGE=docker.io/resys/dialob-session-boot
+
+echo " docker.io/resys/dialob-session-boot:${RELEASE_VERSION}"
+echo " docker.io/resys/dialob-boot:${RELEASE_VERSION}"
+mvn clean -Pjib package jib:docker -pl dialob-session-boot -Djib.to.image=${DIALOB_SESSION_IMAGE}:${RELEASE_VERSION}
+mvn clean -Pjib package jib:docker -pl dialob-boot -Djib.to.image=${DIALOB_BOOT_IMAGE}:${RELEASE_VERSION}
+docker tag ${DIALOB_SESSION_IMAGE}:${RELEASE_VERSION} ${DIALOB_SESSION_IMAGE}:latest
+docker tag ${DIALOB_BOOT_IMAGE}:${RELEASE_VERSION} ${DIALOB_BOOT_IMAGE}:latest
+docker push ${DIALOB_SESSION_IMAGE}:latest
+docker push ${DIALOB_BOOT_IMAGE}:latest
