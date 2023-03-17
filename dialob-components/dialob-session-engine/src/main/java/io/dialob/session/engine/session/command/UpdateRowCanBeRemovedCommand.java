4dbef5a841e547043fe25a8e5f08894d693295e4
Tomi Pakarinen <tomi.pakarinen@iki.fi>
2023-03-17 11:30:24 +0200

    Removed commented code

--- dialob-components/dialob-session-engine/src/main/java/io/dialob/session/engine/session/command/UpdateRowCanBeRemovedCommand.java
+++ dialob-components/dialob-session-engine/src/main/java/io/dialob/session/engine/session/command/UpdateRowCanBeRemovedCommand.java
@@ -24,22 +24,6 @@ import javax.annotation.Nonnull;
 @Value.Immutable
 public interface UpdateRowCanBeRemovedCommand extends AbstractUpdateBooleanAttributeCommand {
 
-
-//  @NotNull
-//  @Override
-//  default Set<EventMatcher> getEventMatchers() {
-//
-//    Set<EventMatcher> eventMatchers = getExpression().getEvalRequiredConditions();
-//    if (getTargetId().isPartial()) {
-//      return Sets.union(eventMatchers, ImmutableSet.of(
-//        EventMatchers.whenItemAdded(getTargetId()),
-//        EventMatchers.whenItemsChanged(getTargetId().getParent().get())
-//      ));
-//    }
-//    return eventMatchers;
-//
-//  }
-
   @Nonnull
   @Override
   default ItemState update(@Nonnull EvalContext context, @Nonnull ItemState itemState) {
