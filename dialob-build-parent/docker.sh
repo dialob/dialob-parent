162434af0acd0acac8c74aff709d2c2fa24fa6f1
Olev Mutso <olev.mutso@resys.io>
2023-03-21 11:35:54 +0200

    Update docker.sh

--- dialob-build-parent/docker.sh
+++ dialob-build-parent/docker.sh
@@ -14,9 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 set -e
-
+echo "github sha: ${GITHUB_SHA}"
+echo "github ref: ${GITHUB_REF}"
 echo "Build and push docker images"
 
 DIALOB_BOOT_IMAGE=docker.io/resys/dialob-boot
