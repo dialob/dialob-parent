132c7f1aab18af3280c59b5d0a67f9689adf9382
Villu Vaimets <villu.vaimets@resys.io>
2023-03-21 12:43:59 +0200

    fixed license header :)

--- dialob-build-parent/docker.sh
+++ dialob-build-parent/docker.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 set -e
 echo "github sha: ${GITHUB_SHA}"
 echo "github ref: ${GITHUB_REF}"
