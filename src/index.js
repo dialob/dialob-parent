89076ddf803d10a7730ed53ebf6e23ca899ccd27
Villu Vaimets <villu.vaimets@resys.io>
2023-04-28 16:09:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 import {combineReducers, applyMiddleware, createStore} from 'redux';
