ddce66285ddf4755a942dc5cf0e6cecab09c69d4
admirkrilasevic <admirkrilasevic@gmail.com>
2023-12-12 10:40:13 +0100

    material composer menu and drawer layout

--- frontend/dialob-composer-material/src/App.tsx
+++ frontend/dialob-composer-material/src/App.tsx
@@ -4,15 +4,17 @@ import { DebugFormView } from './views/DebugFormView';
 
 // TODO: Use built-in test form here
 import testForm from './dialob/test/testForm.json';
+import ComposerLayoutView from './views/ComposerLayoutView';
+import { IntlProvider } from 'react-intl';
+import messages from './intl';
 
 function App() {
- 
+
   return (
     <ComposerProvider formData={testForm}>
-      <Container>
-        <Typography variant='h4'>Dialob Composer</Typography>
-        <DebugFormView />
-      </Container>
+      <IntlProvider locale='en' messages={messages['en']}>
+        <ComposerLayoutView />
+      </IntlProvider>
     </ComposerProvider>
   )
 }
