b1b5114cf4503fb8d6d4165d42a5c8761e0aab7c
admirkrilasevic <admirkrilasevic@gmail.com>
2024-04-12 14:20:03 +0200

    showing search results and click to navigate

--- frontend/dialob-composer-material/src/views/layout/MenuBar.tsx
+++ frontend/dialob-composer-material/src/views/layout/MenuBar.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { AppBar, Box, Divider, InputBase, Stack, Typography, useTheme, Button, Menu, MenuItem, styled } from '@mui/material';
+import { AppBar, Box, Divider, Stack, Typography, useTheme, Button, Menu, MenuItem, styled, TextField, Popover, List } from '@mui/material';
 import { ArrowDropDown, Close, Download, Search, Support, Visibility } from '@mui/icons-material';
 import { FormattedMessage, useIntl } from 'react-intl';
-import { useComposer } from '../../dialob';
+import { isContextVariable, useComposer } from '../../dialob';
 import { getStatusIcon } from '../../utils/ErrorUtils';
 import { useEditor } from '../../editor';
-import { SCROLLBAR_WIDTH } from '../../theme/siteTheme';
+import { SCROLLBAR_WIDTH, SCROLL_SX } from '../../theme/siteTheme';
 import GlobalListsDialog from '../../dialogs/GlobalListsDialog';
 import TranslationDialog from '../../dialogs/TranslationDialog';
 import FormOptionsDialog from '../../dialogs/FormOptionsDialog';
 import VariablesDialog from '../../dialogs/VariablesDialog';
+import { matchItemByKeyword, matchVariableByKeyword } from '../../utils/SearchUtils';
+import { scrollToItem } from '../../utils/ScrollUtils';
+
+interface SearchMatch {
+  type: 'item' | 'variable';
+  id: string;
+}
 
 const ResponsiveButton = styled(Button)(({ theme }) => ({
   [theme.breakpoints.down('lg')]: {
@@ -50,7 +57,7 @@ const MenuBar: React.FC = () => {
   const theme = useTheme();
   const intl = useIntl();
   const { form } = useComposer();
-  const { editor, setActiveFormLanguage } = useEditor();
+  const { editor, setActiveFormLanguage, setActivePage, setHighlightedItem, setActiveVariableTab } = useEditor();
   const headerPaddingSx = { px: theme.spacing(1) };
   const formLanguages = form.metadata.languages || ['en'];
   const [listsDialogOpen, setListsDialogOpen] = React.useState(false);
@@ -58,6 +65,9 @@ const MenuBar: React.FC = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [optionsDialogOpen, setOptionsDialogOpen] = React.useState(false);
   const [variablesDialogOpen, setVariablesDialogOpen] = React.useState(false);
+  const [searchAnchor, setSearchAnchor] = React.useState<null | HTMLElement>(null);
+  const [searchKeyword, setSearchKeyword] = React.useState('');
+  const [searchMatches, setSearchMatches] = React.useState<SearchMatch[]>([]);
   const languageMenuOpen = Boolean(anchorEl);
 
   const handleLanguageMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
@@ -68,6 +78,44 @@ const MenuBar: React.FC = () => {
     setActiveFormLanguage(languageCode);
     setAnchorEl(null);
   }
+
+  const handleMatchClick = (match: SearchMatch) => {
+    setSearchAnchor(null);
+    if (match.type === 'item') {
+      setHighlightedItem(form.data[match.id]);
+      scrollToItem(match.id, Object.values(form.data), editor.activePage, setActivePage);
+    } else {
+      const variable = form.variables?.find(v => v.name === match.id);
+      if (variable) {
+        setActiveVariableTab(isContextVariable(variable) ? 'context' : 'expression');
+      }
+    }
+  }
+
+  React.useEffect(() => {
+    if (searchKeyword.length === 0) {
+      setSearchMatches([]);
+      return;
+    }
+    const id = setTimeout(() => {
+      const matches: SearchMatch[] = [];
+      for (const item of Object.values(form.data)) {
+        if (matchItemByKeyword(item, form.metadata.languages, searchKeyword)) {
+          matches.push({ type: 'item', id: item.id });
+        }
+      }
+      if (form.variables) {
+        for (const variable of form.variables) {
+          if (matchVariableByKeyword(variable, searchKeyword)) {
+            matches.push({ type: 'variable', id: variable.name });
+          }
+        }
+      }
+      setSearchMatches(matches);
+    }, 500);
+    return () => clearTimeout(id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchKeyword]);
 
   return (
     <>
@@ -92,10 +140,32 @@ const MenuBar: React.FC = () => {
           <HeaderButton label={intl.formatMessage({ id: 'version' }) + ": " + intl.formatMessage({ id: 'version.latest' })} endIcon={<ArrowDropDown />} />
           <HeaderIconButton icon={<Support fontSize='small' />} />
           <Box flexGrow={1} />
-          <Box sx={{ display: 'flex', alignItems: 'center', ...headerPaddingSx }}>
-            <InputBase placeholder={intl.formatMessage({ id: 'search' })} />
-            <Search />
+          <Box sx={{ display: 'flex', alignItems: 'center', ...headerPaddingSx }} onClick={(e) => setSearchAnchor(e.currentTarget)}>
+            <TextField
+              variant='standard'
+              placeholder={intl.formatMessage({ id: 'search' })}
+              InputProps={{ endAdornment: <Search />, disableUnderline: true }}
+              value={searchKeyword}
+              onChange={(e) => setSearchKeyword(e.target.value)} />
           </Box>
+          <Popover open={Boolean(searchAnchor)} anchorEl={searchAnchor} onClose={() => setSearchAnchor(null)} anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }} disableAutoFocus disableScrollLock>
+            <List sx={{ maxHeight: '50vh', ...SCROLL_SX }}>
+              {searchMatches.length === 0 && <MenuItem>
+                <Typography color='text.hint'>Enter search keyword</Typography>
+              </MenuItem>}
+              {searchMatches
+                .sort((a, b) => a.type.localeCompare(b.type))
+                .map((match) => (
+                  <MenuItem key={match.id} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }} onClick={() => handleMatchClick(match)}>
+                    <Typography fontWeight='bold' color={match.type === 'variable' ? 'primary' : 'inherit'}>{match.id}</Typography>
+                    <Typography color='text.hint'>{match.type}</Typography>
+                  </MenuItem>
+                ))}
+            </List>
+          </Popover>
           <HeaderIconButton icon={<Download />} />
           <HeaderIconButton disabled icon={getStatusIcon(editor.errors)} />
           <HeaderButton label={'locales.' + editor.activeFormLanguage} endIcon={<ArrowDropDown />} onClick={handleLanguageMenuOpen} />
