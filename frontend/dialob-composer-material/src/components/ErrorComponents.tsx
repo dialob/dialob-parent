68281616f9f50366c0a7dc46597973c05bc35219
admirkrilasevic <admirkrilasevic@gmail.com>
2024-05-13 11:51:42 +0200

    extract code from error

--- frontend/dialob-composer-material/src/components/ErrorComponents.tsx
+++ frontend/dialob-composer-material/src/components/ErrorComponents.tsx
@@ -1,5 +1,6 @@
 import { useIntl } from "react-intl";
 import { EditorError } from "../editor";
+import { DialobItem, Variable, useComposer } from "../dialob";
 
 export const ErrorType: React.FC<{ error: EditorError }> = ({ error }) => {
   const intl = useIntl();
@@ -20,15 +21,44 @@ export const ErrorType: React.FC<{ error: EditorError }> = ({ error }) => {
   return <>{resolveType() + ' ' + intl.formatMessage({ id: 'errors.title' })}</>;
 }
 
+const extractCodeFromItem = (item: DialobItem, start: number, end: number, type: string) => {
+  switch (type) {
+    case 'VISIBILITY':
+      return item.activeWhen!.substring(start, end + 1);
+    case 'REQUIREMENT':
+      return item.required!.substring(start, end + 1);
+    default:
+      return '';
+  }
+}
+
+const extractCodeFromVariable = (variable: Variable, start: number, end: number) => {
+  return variable.expression.substring(start, end + 1);
+}
+
 export const ErrorMessage: React.FC<{ error: EditorError }> = ({ error }) => {
   const intl = useIntl();
-  if (error.message === 'VALUESET_DUPLICATE_KEY') {
-    return intl.formatMessage({ id: 'errors.message.VALUESET_DUPLICATE_KEY' }, { expression: error.expression });
+  const { form } = useComposer();
+  const { message, expression, type, startIndex, endIndex, itemId } = error;
+  let msg = message;
+  let code = '';
+  if (message === 'VALUESET_DUPLICATE_KEY') {
+    msg = intl.formatMessage({ id: 'errors.message.VALUESET_DUPLICATE_KEY' }, { expression: expression });
   }
-  const label = `errors.message.${error.message}`;
+  const label = `errors.message.${message}`;
   const stringExists = !!intl.messages[label];
   if (stringExists) {
-    return intl.formatMessage({ id: label });
+    msg = intl.formatMessage({ id: label });
   }
-  return error.message;
+  if (itemId !== undefined && startIndex !== undefined && endIndex !== undefined && type !== undefined) {
+    if (type === 'VARIABLE') {
+      const variable = form.variables?.find(v => v.name === itemId);
+      if (variable) {
+        code = extractCodeFromVariable(variable as Variable, startIndex, endIndex);
+      }
+    } else {
+      code = extractCodeFromItem(form.data[itemId], startIndex, endIndex, type);
+    }
+  }
+  return <>{msg} <i>{code}</i></>;
 }
