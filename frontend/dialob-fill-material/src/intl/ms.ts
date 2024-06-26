b260fedce80b83b854d1ac8a92afd90a32e4ad94
Mikki <mika.lindholm@resys.io>
2024-06-26 08:36:57 +0300

    Add files via upload
    Malay translation

---
+++ frontend/dialob-fill-material/src/intl/ms.ts
@@ -0,0 +1,24 @@
+const ms = {
+  'yes': 'Ya',
+  'no': 'Tidak',
+
+  'session.not.found': 'Sessi tidak ditemui!',
+  'form.completed': 'Borang telah siap diisi!',
+  'page.previous': 'Halaman sebelumnya',
+  'page.next': 'Muka surat seterusnya',
+  'complete': 'Lengkap,
+  'complete.confirmation.title': 'Adakan anda pasti untuk menglengkapkan dialog ini?',
+  'complete.confirmation.message': 'Anda tidak dapat menukar jawapan and selepas selesai',
+  'complete.confirmation.cancel': 'Batal',
+  'complete.confirmation.confirm': 'Mengesahkan',
+
+  'row.add.button': 'Tambah',
+  'row.remove.button': 'Keluarkan',
+  'row.remove.confirmation.title': 'Adakah anda ingin mengalih keluar barisan itu?',
+  'row.remove.confirmation.cancel': 'Batalkan',
+  'row.remove.confirmation.confirm': 'Keluarkan,
+
+  'autocomplete.nooptions': 'Tiada pilihan'
+};
+
+export default ms;
