e53bdf049ede299e6bf5151057156d32fdb4036c
Villu Vaimets <villu.vaimets@resys.io>
2024-06-28 09:05:54 +0300

    include malaysian

--- frontend/dialob-fill-material/src/intl/index.ts
+++ frontend/dialob-fill-material/src/intl/index.ts
@@ -2,12 +2,14 @@ import en from './en';
 import fi from './fi';
 import sv from './sv';
 import et from './et';
+import ms from './ms';
 
 const messages: {[key: string]: any}  = {
   en,
   fi,
   sv,
-  et
+  et,
+	ms
 };
 
 export default messages;
