1ff79d9e7322ee2a13a3cb5b20157110be548893
Tomi Pakarinen <tomi.pakarinen@resys.io>
2024-09-18 15:28:18 +0300

    Cleanup old CICD files

--- frontend/dialob-fill-demo-material/aws-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-aws s3 sync ./build s3://cdn.resys.io/dialob/dialob-fill-demo-material/main --exclude '*.html' --delete --cache-control public,max-age=3600 --acl public-read 
-aws s3 sync ./build s3://cdn.resys.io/dialob/dialob-fill-demo-material/main --include '*.html' --cache-control no-cache --acl public-read
-
