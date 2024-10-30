63cd63ba1c3ac7d594d358a4b97a5b53e4319e0c
emir.karaman <emir.karaman@resys.io>
2024-10-30 13:49:48 +0100

    added mui alert wrapper around the note item

--- frontend/dialob-fill-material/src/components/Note.tsx
+++ frontend/dialob-fill-material/src/components/Note.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import { ItemAction } from '@dialob/fill-api';
 import { MarkdownView } from './MarkdownView';
-import { Box } from '@mui/material';
+import { Alert, Box } from '@mui/material';
 import { getLayoutStyleFromProps } from './helpers';
 import { DescriptionWrapper } from './DescriptionWrapper';
 
 export interface NoteProps {
   note: ItemAction<'note'>['item'];
-};
+}
+
+const validSeverities = ["error", "warning", "success", "info"];
+
+const getAlertSeverity = (style?: string, view?: string): any => (
+  view === "validation" && (!style || !validSeverities.includes(style))
+    ? "error"
+    : ( style && validSeverities.includes(style) ? style : undefined)
+);
 
 export const Note: React.FC<NoteProps> = ({ note }) => {
+  const severity = getAlertSeverity(note?.props?.style, note?.view);
 
   return (
     <DescriptionWrapper text={note.description} title={note.label}>
       <Box sx={getLayoutStyleFromProps(note.props)}>
-        <MarkdownView text={note.label} />
+        {severity ? (
+          <Alert variant="outlined" severity={severity}>
+            <MarkdownView text={note.label} />
+          </Alert>
+        ) : (
+          <MarkdownView text={note.label} />
+        )}
       </Box>
     </DescriptionWrapper>
-    );
+  );
 };
