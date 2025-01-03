4144cafe7237286536aca36a1ab748e277b4dc52
Admir Krilašević <admirkrilasevic@gmail.com>
2025-01-03 14:26:23 +0100

    Composer customization config fixes (#273)
    * propagate ItemConfig to editor
    
    * make config interfaces more fine grained
    
    * fix custom config to concat existing items

--- frontend/dialob-composer-material/src/components/Editor.tsx
+++ frontend/dialob-composer-material/src/components/Editor.tsx
@@ -5,27 +5,30 @@ import { useEditor } from '../editor';
 import Items from '../items';
 import PageTabs from './PageTabs';
 import { AddItemMenu } from '../items/ItemComponents';
+import { ItemConfig } from '../defaults/types';
+import { useBackend } from '../backend/useBackend';
 
 
-const createChildren = (items: DialobItems, activePage?: DialobItem) => {
+const createChildren = (items: DialobItems, activePage?: DialobItem, itemConfig?: ItemConfig) => {
   if (!activePage || !activePage.items) {
     return null;
   }
   return (
     activePage.items
       .map((itemId: string) => items[itemId])
-      .map((item: DialobItem) => Items.itemFactory(item))
+      .map((item: DialobItem) => Items.itemFactory(item, itemConfig))
   );
 };
 
 const Editor: React.FC = () => {
   const { form } = useComposer();
   const { editor } = useEditor();
+  const { config } = useBackend();
 
   return (
     <Box id='scroll-container'>
       <PageTabs items={form.data} />
-      {createChildren(form.data, editor.activePage)}
+      {createChildren(form.data, editor.activePage, config.itemEditors)}
       {editor.activePage && <Box sx={{ mb: 2 }}><AddItemMenu item={editor.activePage} /></Box>}
     </Box>
   );
