aafea9a76147252dbd2641912d0fd43ceb259a6b
emirkaraman187 <148436453+emirkaraman187@users.noreply.github.com>
2025-01-15 14:52:34 +0100

    adding tenantId to the dialob dashboard config (#282)

--- frontend/dialob-material-dashboard/src/index.tsx
+++ frontend/dialob-material-dashboard/src/index.tsx
@@ -1,16 +1,17 @@
 import { DialobAdmin } from './DialobAdmin'
 
 interface CsrfShape {
-	'key': string,
-	'value': string
+  'key': string,
+  'value': string
 }
 
 interface DialobAdminConfig {
-	dialobApiUrl: string;
-	setLoginRequired: () => void;
-	setTechnicalError: () => void;
-	language: string;
-	csrf: CsrfShape | undefined;
+  dialobApiUrl: string;
+  setLoginRequired: () => void;
+  setTechnicalError: () => void;
+  language: string;
+  csrf: CsrfShape | undefined;
+  tenantId?: string;
 }
 
 export { DialobAdmin, type DialobAdminConfig }
