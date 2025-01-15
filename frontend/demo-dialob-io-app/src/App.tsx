316d97c209360e773ef15daa3254dc8e8e03f716
emir.karaman <emir.karaman@resys.io>
2025-01-15 08:51:20 +0100

    added some basic configuration for dialob admin io

---
+++ frontend/demo-dialob-io-app/src/App.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { ComposerProvider, ComposerState } from './dialob'
+import { useEditor } from './editor';
+import { IntlProvider } from 'react-intl';
+import messages from './intl';
+import ComposerLayoutView from './views/ComposerLayoutView';
+import { Dispatch } from 'react';
+import { CircularProgress, Grid } from '@mui/material';
+import { Middleware } from './dialob/react/ComposerContext';
+import { ComposerAction } from './dialob/actions';
+import { useBackend } from './backend/useBackend';
+import { SaveResult } from './backend/types';
+
+export const ProgressSplash: React.FC = () => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh' }}
+    >
+      <Grid item xs={3}>
+        <CircularProgress size={100} thickness={5} />
+      </Grid>
+    </Grid>
+  )
+}
+
+function App() {
+  const { form, loaded, saveForm } = useBackend();
+  const { setErrors } = useEditor();
+
+  async function saveFormMiddleware(action: ComposerAction | undefined, state: ComposerState, dispatch: Dispatch<ComposerAction>) {
+    if (action && action.type !== 'setRevision' && state._tag === undefined) {
+      if (action.type === 'setForm' && !action.save) {
+        return;
+      }
+      saveForm(state)
+        .then(saveResponse => {
+          if (saveResponse.success && saveResponse.result) {
+            const result = saveResponse.result as SaveResult;
+            const errors = result.errors?.map(e => {
+              if (e.itemId && e.itemId.includes(':')) {
+                const itemId = e.itemId.split(':')[0];
+                return { ...e, itemId: itemId };
+              }
+              return e;
+            }); setErrors(errors);
+            dispatch({ type: 'setRevision', revision: result.rev });
+          } else if (saveResponse.apiError) {
+            setErrors([{ level: 'FATAL', message: saveResponse.apiError.message }])
+          }
+        });
+    }
+  }
+
+  if (form === null || !loaded) {
+    return (
+      <ProgressSplash />
+    );
+  }
+
+  const preMiddleware: Middleware[] = [];
+
+  const postMiddleware: Middleware[] = [
+    saveFormMiddleware,
+  ];
+
+  return (
+    <ComposerProvider formData={form} preMiddleware={preMiddleware} postMiddleware={postMiddleware}>
+      <IntlProvider locale='en' messages={messages['en']}>
+        <ComposerLayoutView />
+      </IntlProvider>
+    </ComposerProvider>
+  )
+}
+
+export default App;
---
+++ frontend/demo-dialob-io-app/src/main.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { siteTheme } from './theme/siteTheme'
+import { AppConfig, DialobComposerConfig } from './backend/types'
+import DialobComposer from './dialob/DialobComposer'
+
+
+const renderDialobComposer = (targetElement: HTMLElement, appConfig: AppConfig) => {
+
+  const FORM_ID = appConfig.formId;
+
+  const baseUrl = window.location.origin;
+
+  const DIALOB_COMPOSER_CONFIG: DialobComposerConfig = {
+    transport: {
+      csrf: appConfig.csrfHeader ? {
+        headerName: appConfig.csrfHeader,
+        token: appConfig.csrf
+      } : undefined,
+      apiUrl: appConfig.backend_api_url.includes('://') ? appConfig.backend_api_url : baseUrl + appConfig.backend_api_url,
+      previewUrl: appConfig.filling_app_url,
+      tenantId: appConfig.tenantId || undefined,
+      credentialMode: appConfig.credentialMode || undefined,
+    },
+    backendVersion: appConfig.version,
+    documentationUrl: 'https://github.com/dialob/dialob-parent/wiki/',
+    closeHandler: () => window.location.href = appConfig.adminAppUrl,
+  };
+
+  ReactDOM.createRoot(targetElement!).render(
+    <React.StrictMode>
+      <ThemeProvider theme={siteTheme}>
+        <CssBaseline />
+        <DialobComposer config={DIALOB_COMPOSER_CONFIG} formId={FORM_ID} />
+      </ThemeProvider>
+    </React.StrictMode>,
+  )
+};
+
+declare global {
+  interface Window {
+    renderDialobComposer: (targetElement: HTMLElement, appConfig: AppConfig) => void;
+  }
+}
+
+window.renderDialobComposer = renderDialobComposer;
