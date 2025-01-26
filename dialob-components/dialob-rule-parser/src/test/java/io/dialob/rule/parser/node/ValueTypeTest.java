3a14a0cad0340e653f42db451034352e866a8bb2
Tomi Pakarinen <tomi.pakarinen@resys.io>
2025-01-26 12:09:49 +0200

    Remove unused Variable class and enhance ValueType tests
    The Variable class was deleted as it is no longer in use. Additional null-check assertions were added to ValueType tests to ensure proper initialization and improve test coverage for various value types.

--- dialob-components/dialob-rule-parser/src/test/java/io/dialob/rule/parser/node/ValueTypeTest.java
+++ dialob-components/dialob-rule-parser/src/test/java/io/dialob/rule/parser/node/ValueTypeTest.java
@@ -20,8 +20,16 @@ public class ValueTypeTest {
 
   public static final BigInteger ONE = BigInteger.ONE;
 
+
+  @Test
+  public void dateIsParseable() {
+    assertNotNull(ValueType.DATE, "ValueType.DATE should not be null");
+    assertNotNull(ValueType.DATE, "ValueType.DATE should not be null");
+    assertDoesNotThrow(() -> ValueType.DATE.parseFromString("2025-01-01"));
+  }
   @Test
   public void booleanIsParseAble() throws Exception {
+    assertNotNull(ValueType.BOOLEAN, "ValueType.BOOLEAN should not be null");
     assertNull(ValueType.BOOLEAN.parseFromString(null));
     assertTrue((Boolean) ValueType.BOOLEAN.parseFromString("true"));
     assertFalse((Boolean) ValueType.BOOLEAN.parseFromString("false"));
@@ -38,6 +46,7 @@ public class ValueTypeTest {
 
   @Test
   public void integerIsParseable() throws Exception {
+    assertNotNull(ValueType.INTEGER, "ValueType.INTEGER should not be null");
     assertNull(ValueType.INTEGER.parseFromString(null));
     Assertions.assertEquals(ZERO, ValueType.INTEGER.parseFromString("0"));
   }
@@ -55,30 +64,35 @@ public class ValueTypeTest {
 
   @Test
   public void decimalIsParseAble() throws Exception {
+    assertNotNull(ValueType.DECIMAL, "ValueType.DECIMAL should not be null");
     assertNull(ValueType.DECIMAL.parseFromString(null));
     Assertions.assertEquals(BigDecimal.valueOf(0.1), ValueType.DECIMAL.parseFromString("0.1"));
   }
 
   @Test
   public void dateIsParseAble() throws Exception {
+    assertNotNull(ValueType.DATE, "ValueType.DATE should not be null");
     assertNull(ValueType.DATE.parseFromString(null));
     Assertions.assertEquals(LocalDate.of(2016, 2, 1), ValueType.DATE.parseFromString("2016-02-01"));
   }
 
   @Test
   public void timeIsParseAble() throws Exception {
+    assertNotNull(ValueType.TIME, "ValueType.TIME should not be null");
     assertNull(ValueType.TIME.parseFromString(null));
     Assertions.assertEquals(LocalTime.of(23, 45, 10), ValueType.TIME.parseFromString("23:45:10"));
   }
 
   @Test
   public void periodIsParseAble() throws Exception {
+    assertNotNull(ValueType.PERIOD, "ValueType.PERIOD should not be null");
     assertNull(ValueType.PERIOD.parseFromString(null));
     Assertions.assertEquals(Period.of(10, 3, 15), ValueType.PERIOD.parseFromString("P10Y3M15D"));
   }
 
   @Test
   public void durationIsParseAble() throws Exception {
+    assertNotNull(ValueType.DURATION, "ValueType.DURATION should not be null");
     assertNull(ValueType.DURATION.parseFromString(null));
     Assertions.assertEquals(Duration.ofDays(100).plusHours(2), ValueType.DURATION.parseFromString("P100DT2H"));
   }
