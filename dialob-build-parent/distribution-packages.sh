1d04c607eaeb404832d903125b5659e50c234d3c
Tomi Pakarinen <tomi.pakarinen@resys.io>
2025-04-01 16:49:57 +0300

    locate dist.sh script

--- dialob-build-parent/distribution-packages.sh
+++ dialob-build-parent/distribution-packages.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
 export CDN_DOMAIN=cdn.resys.io
 export GITHUB_REPOSITORY_OWNER=dialob
+
+DIST=$SCRIPT_DIR/dist.sh
 
 if [ -z "$PUBLISHED_PACKAGES" ]; then
   for PROJECT in $(pnpm -r ls --json | jq -r '.[] | .path'); do
     echo $PROJECT
-    ./dialob-build-parent/dist.sh $PROJECT $*
+    $DIST $PROJECT $*
   done
 else
   for PROJECT in `comm -12 <(echo $PUBLISHED_PACKAGES | jq -r '.[] | .name') <(pnpm m ls --json | jq -r '.[] | select(.private) | .name')`; do
-    ./dialob-build-parent/dist.sh -r $(pnpm -r --filter=$PROJECT ls --json | jq -r '.[] | .path') $*
+    $DIST -r $(pnpm -r --filter=$PROJECT ls --json | jq -r '.[] | .path') $*
   done
 fi
 
