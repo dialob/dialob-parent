0150464f0ac0ce0b88349eff8a3d7787ad8ee622
Tomi Pakarinen <tomi.pakarinen@resys.io>
2025-04-01 19:14:13 +0300

    Drop old aws publish commands

--- frontend/dialob-composer-material/aws-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash                        
-aws s3 sync ./dist s3://cdn.resys.io/dialob-composer-material/dev --exclude '*.html' --delete --cache-control public,max-age=3600 --acl public-read 
-aws s3 sync ./dist s3://cdn.resys.io/dialob-composer-material/dev --include '*.html' --cache-control no-cache --acl public-read
-
