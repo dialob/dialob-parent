49d8b2757c1325441ab58d231ff883d7161f0d3d
Tomi Pakarinen <tomi.pakarinen@resys.io>
2025-04-01 16:40:27 +0300

    Distribution packages

---
+++ dialob-build-parent/dist.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -o|--outdir)
+      OUTDIR="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -r|--release)
+      MODE="package"
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+PROJECT_DIR=$1
+
+if [ -z "$PROJECT_DIR" ]; then
+  echo "No project dir set."
+  exit 1
+fi
+
+PACKAGE_JSON=$PROJECT_DIR/package.json
+
+if [ ! -e $PACKAGE_JSON ]; then
+  echo "$PACKAGE_JSON does not exists."
+  exit 1
+fi
+
+HAS_DIST=`jq '.scripts | has("dist")' < $PACKAGE_JSON`
+if [ "$HAS_DIST" = "false" ]; then
+  echo "$(basename $PROJECT_DIR): No dist target "
+  exit 0
+fi
+
+if [ -z "$OUTDIR" ]; then
+  echo "No output folder set."
+  exit 1
+else
+  mkdir -p $OUTDIR
+fi
+
+NAME=$(jq -r .name $PROJECT_DIR/package.json)
+
+case $MODE in
+package)
+  VERSION=$(jq -r .version $PROJECT_DIR/package.json)
+  ;;
+*)
+  VERSION=$GITHUB_REF_NAME
+  ;;
+esac
+
+if [ -z "$VERSION" ]; then
+  echo "Version not set."
+  exit 1
+fi
+
+PROJECT_NAME=$(echo "$NAME" | awk -F'/' '{print $NF}')
+
+cd $PROJECT_DIR
+DIST_BASE=https://${CDN_DOMAIN}/${GITHUB_REPOSITORY_OWNER}/${PROJECT_NAME}/${VERSION} pnpm run dist
+cd -
+
+if [ -e "$PROJECT_DIR/dist" ]; then
+  gtar vzcf $OUTDIR/$PROJECT_NAME-$VERSION.tar.gz --transform "s|^.|$PROJECT_NAME/$VERSION|" --show-stored-names -C $PROJECT_DIR/dist .
+fi
--- dialob-build-parent/distribution-packages.sh
+++ dialob-build-parent/distribution-packages.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-for PROJECT in `comm -12 <(echo $PUBLISHED_PACKAGES | jq -r '.[] | .name') <(pnpm m ls --json | jq -r '.[] | select(.private) | .name')`; do
-  pnpm -r --filter=$PROJECT run dist
-done
+export CDN_DOMAIN=cdn.resys.io
+export GITHUB_REPOSITORY_OWNER=dialob
+
+if [ -z "$PUBLISHED_PACKAGES" ]; then
+  for PROJECT in $(pnpm -r ls --json | jq -r '.[] | .path'); do
+    echo $PROJECT
+    ./dialob-build-parent/dist.sh $PROJECT $*
+  done
+else
+  for PROJECT in `comm -12 <(echo $PUBLISHED_PACKAGES | jq -r '.[] | .name') <(pnpm m ls --json | jq -r '.[] | select(.private) | .name')`; do
+    ./dialob-build-parent/dist.sh -r $(pnpm -r --filter=$PROJECT ls --json | jq -r '.[] | .path') $*
+  done
+fi
+
+
+
