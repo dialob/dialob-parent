873f8cf9b79ff3be910347cbb626d03a317707bf
Tomi Pakarinen <tomi.pakarinen@resys.io>
2025-04-02 09:37:53 +0300

    Skip publication if no artifacts was created

--- frontend/distribution-packages.sh
+++ frontend/distribution-packages.sh
@@ -4,16 +4,20 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 DIST=$SCRIPT_DIR/dist.sh
 
+OUTPUT=()
+
 if [ -z "$PUBLISHED_PACKAGES" ] || [ "$PUBLISHED_PACKAGES" = "[]" ]; then
   for PROJECT in $(pnpm -r ls --json | jq -r '.[] | .path'); do
-    echo $PROJECT
     $DIST $PROJECT $*
+    OUTPUT+="$PROJECT"
   done
 else
   for PROJECT in `comm -12 <(echo $PUBLISHED_PACKAGES | jq -r '.[] | .name') <(pnpm m ls --json | jq -r '.[] | select(.private) | .name')`; do
     $DIST -r $(pnpm -r --filter=$PROJECT ls --json | jq -r '.[] | .path') $*
+    OUTPUT+="$PROJECT
   done
 fi
 
-
-
+if [ -e "$GITHUB_OUTPUT" ]; then
+  echo "artifacts=$(jq --compact-output --null-input '[ $ARGS.positional | .[] | select( . != "" ) ]' --args  -- "${OUTPUT[@]}")" >> $GITHUB_OUTPUT
+fi
