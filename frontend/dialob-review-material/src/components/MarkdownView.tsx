d66e1a1db69f541b12f700f90fb5308abbf133b4
Tomi Pakarinen <tomi.pakarinen@resys.io>
2025-04-07 11:07:27 +0300

    PropsWithChildren does not need empty type parameter

--- frontend/dialob-review-material/src/components/MarkdownView.tsx
+++ frontend/dialob-review-material/src/components/MarkdownView.tsx
@@ -21,15 +21,15 @@ const HLEVEL_MAPPING: ('h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6')[] = [
 ];
 
 const HeadingRenderer: React.FC<PropsWithChildren<HeadingRendererProps>> = ({ children, level }) => <Typography variant={HLEVEL_MAPPING[level - 1]} gutterBottom>{children}</Typography>;
-const ParagraphRenderer: React.FC<PropsWithChildren<{}>> = ({ children }) => <Typography variant='body1' paragraph>{children}</Typography>;
+const ParagraphRenderer: React.FC<PropsWithChildren> = ({ children }) => <Typography variant='body1' paragraph>{children}</Typography>;
 const LinkRenderer: React.FC<PropsWithChildren<LinkRendererProps>> = ({ children, href }) => <Link href={href}>{children}</Link>;
-const TableRenderer: React.FC<PropsWithChildren<{}>> = ({ children }) => <Table>{children}</Table>;
-const TableHeadRenderer: React.FC<PropsWithChildren<{}>> = ({ children }) => <TableHead>{children}</TableHead>;
-const TableBodyRenderer: React.FC<PropsWithChildren<{}>> = ({ children }) => <TableBody>{children}</TableBody>;
-const TableRowRenderer: React.FC<PropsWithChildren<{}>> = ({ children }) => <TableRow>{children}</TableRow>;
-const TableCellRenderer: React.FC<PropsWithChildren<{}>> = ({ children }) => <TableCell>{children}</TableCell>;
+const TableRenderer: React.FC<PropsWithChildren> = ({ children }) => <Table>{children}</Table>;
+const TableHeadRenderer: React.FC<PropsWithChildren> = ({ children }) => <TableHead>{children}</TableHead>;
+const TableBodyRenderer: React.FC<PropsWithChildren> = ({ children }) => <TableBody>{children}</TableBody>;
+const TableRowRenderer: React.FC<PropsWithChildren> = ({ children }) => <TableRow>{children}</TableRow>;
+const TableCellRenderer: React.FC<PropsWithChildren> = ({ children }) => <TableCell>{children}</TableCell>;
 const DividerRenderer: React.FC = () => <Divider variant='middle' />;
-const ListRenderer: React.FC<PropsWithChildren<{}>> = ({ children }) => <Typography variant='body1'>{children}</Typography>;
+const ListRenderer: React.FC<PropsWithChildren> = ({ children }) => <Typography variant='body1'>{children}</Typography>;
 
 const renderers = {
   heading: HeadingRenderer,
