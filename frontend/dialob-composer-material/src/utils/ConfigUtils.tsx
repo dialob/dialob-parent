69d89b9167d6da0ca71b03db223e1b99725987a3
admirkrilasevic <admirkrilasevic@gmail.com>
2025-04-24 11:58:00 +0200

    fix getting conversion options

--- frontend/dialob-composer-material/src/utils/ConfigUtils.tsx
+++ frontend/dialob-composer-material/src/utils/ConfigUtils.tsx
@@ -10,3 +10,25 @@ export const findItemTypeConfig = (itemTypes: ItemTypeConfig, type: string, view
   }
   return null;
 }
+
+export const findItemTypeConvertible = (itemTypes: ItemTypeConfig, view: string) => {
+  for (const idx in itemTypes.categories) {
+    const c = itemTypes.categories[idx];
+    const resultConfig = c.items.find(v => v.config.type === view || v.config.view === view);
+    if (resultConfig) {
+      return resultConfig;
+    }
+  }
+  return null;
+}
+
+export const findItemPropEditor = (itemTypes: ItemTypeConfig, view: string) => {
+  for (const idx in itemTypes.categories) {
+    const c = itemTypes.categories[idx];
+    const resultConfig = c.items.find(v => v.config.type === view || v.config.view === view);
+    if (resultConfig && resultConfig.propEditors) {
+      return resultConfig.propEditors;
+    }
+  }
+  return undefined;
+}
