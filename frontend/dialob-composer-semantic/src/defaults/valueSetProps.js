c3b331080e31da5eb16c43a7f08c72c5e999c202
Tomi Pakarinen <63479+patope@users.noreply.github.com>
2025-06-06 17:19:24 +0300

    Remove dialob-composer-semantic and dialob-composer-semantic-app (#407)

--- frontend/dialob-composer-semantic/src/defaults/valueSetProps.js
+++
@@ -1,9 +0,0 @@
-import GenericValueSetPropEditor from '../components/GenericValueSetPropEditor';
-
-export const DEFAULT_VALUESET_PROPS = [
-  {
-    title: 'Custom attribute',
-    name: 'attr',
-    editor: GenericValueSetPropEditor
-  }
-];
