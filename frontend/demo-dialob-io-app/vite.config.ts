96d17796ce218fa42a9fbc61c9c04108f38f87bc
emir.karaman <emir.karaman@resys.io>
2025-07-09 10:12:07 +0200

    fixed a mui x date pickers error showing in demo dialob io app

--- frontend/demo-dialob-io-app/vite.config.ts
+++ frontend/demo-dialob-io-app/vite.config.ts
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: [
+      'react',
+      'react-dom',
+      '@mui/material',
+      '@emotion/react',
+      '@emotion/styled',
+      '@mui/icons-material',
+      '@mui/x-date-pickers'
+    ]
+  },
   server: {
     port: 3000,
     /*
